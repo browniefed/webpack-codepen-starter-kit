b0a037d5d950b73cc84ebb1094289806f09cedc7
Jason Brown <iam@browniefed.com>
2015-05-27 19:54:04 -0700

    Fix up the stuff to have no router, and clean up package.json

---
+++ app/Demo.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+
+var Demo = React.createClass({
+	render: function() {
+		return (
+			<div>
+			</div>
+		)
+	}
+});
+
+module.exports = Demo;
--- config/app.jsx
+++ config/app.jsx
@@ -1,10 +1,4 @@
-var React = require("react");
-var Router = require("react-router");
-var routes = require("../app/" + __resourceQuery.substr(1) + "Routes");
-var ReactUpdates = require("react/lib/ReactUpdates");
+var React = require("react"),
+	Demo = require('../app/Demo');
 
-var initialRun = true;
-
-Router.run(routes, Router.HistoryLocation, function(Application, state) {
-	React.render(<Application />, document.getElementById("content"));
-});
+React.render(<Demo />, document.getElementById("content"));
